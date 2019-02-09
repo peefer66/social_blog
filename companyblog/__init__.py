08b7d801ad2db2a91fd46600731427e299b926f1
peefer66 <peefer1966@gmail.com>
2019-02-09 17:20:53 +0000

    13/107 error_pages

--- companyblog/__init__.py
+++ companyblog/__init__.py
@@ -5,5 +5,7 @@ app = Flask(__name__)
 
 # Register Blueprints
 from companyblog.core.views import core
+from companyblog.error_pages.handlers import error_pages
 app.register_blueprint(core)
+app.register_blueprint(error_pages)
 
